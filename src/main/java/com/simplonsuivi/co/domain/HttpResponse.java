baafdc2e58bd3c03b1dafc1045608421bf0c17e3
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-06-28 01:53:25 +0000

    Customisation des responses HTTP

---
+++ src/main/java/com/simplonsuivi/co/domain/HttpResponse.java
@@ -0,0 +1,19 @@
+package com.simplonsuivi.co.domain;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponse {
+	
+	private int httpStatusCode;
+	private HttpStatus httpStatus;
+	private String reason;
+	private String message;
+
+}
--- src/main/java/com/simplonsuivi/co/domain/User.java
+++ src/main/java/com/simplonsuivi/co/domain/User.java
@@ -1,5 +1,41 @@
 package com.simplonsuivi.co.domain;
 
-public class User {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String profileImageUrl;
+	private Date lastLoginDate;
+	private Date lastLoginDateDisplay;
+	private Date joinDate;
+	private String[] roles; //On a un tableau de role (ROLE_USER , ROLE_ADMIN)
+    private String authorities;
+    private boolean isActive;
+    private boolean isNotlocked; 
 }
