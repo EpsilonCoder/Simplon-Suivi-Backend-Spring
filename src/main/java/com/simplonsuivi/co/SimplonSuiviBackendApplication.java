8e6bdc2b82738d26bdc280285af46b95f352b9f1
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-06-28 01:27:44 +0000

    Ajout de l'entité  utilisateurs

---
+++ src/main/java/com/simplonsuivi/co/SimplonSuiviBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simplonsuivi.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplonSuiviBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplonSuiviBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplonsuivi/co/domain/User.java
@@ -0,0 +1,5 @@
+package com.simplonsuivi.co.domain;
+
+public class User {
+
+}
